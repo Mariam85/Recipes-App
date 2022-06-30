69126b66d560f646c41f0da12ffcb2193965daa6
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-06-30 12:54:19 +0200

    Added constructor

--- Recipe.cs
+++ Recipe.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ConsoleApp
+public class Recipe
 {
-    public class Recipe
-    {
-        public List<string> Ingredients { get; set; }
-        public string Title { get; set; }
-        public List<string> Instructions { get; set; }
-        public List<string> Categories { get; set; }
+    public List<string> Ingredients { get; set; }
+    public string Title { get; set; }
+    public List<string> Instructions { get; set; }
+    public List<string> Categories { get; set; }
 
+    public Recipe(List<string> ingredients, string title, List<string> instructions, List<string> categories)
+    {
+        this.Ingredients = ingredients;
+        this.Title = title;
+        this.Instructions = instructions;
+        this.Categories = categories;
     }
 }
