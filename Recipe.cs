f33900a123fc89494b85bcb9b2b3c93ed38367f5
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-07-04 23:32:21 +0200

    Added an Id

--- Recipe.cs
+++ Recipe.cs
@@ -4,13 +4,15 @@ using System.Text;
 
 public class Recipe
 {
-    public List<string> Ingredients { get; set; }
+    public Guid Id { get; set; }
+    public List<string> Ingredients { get; set; }= new List<string>();
     public string Title { get; set; }
-    public List<string> Instructions { get; set; }
-    public List<string> Categories { get; set; }
+    public List<string> Instructions { get; set; }= new List<string>();
+    public List<string> Categories { get; set; }= new List<string>();
 
     public Recipe(List<string> ingredients, string title, List<string> instructions, List<string> categories)
     {
+        Id = Guid.NewGuid();
         this.Ingredients = ingredients;
         this.Title = title;
         this.Instructions = instructions;
