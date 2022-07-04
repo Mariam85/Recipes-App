5fdd7e24d41d7b27d7006103eee37b9fe64ab571
Mariam Mostafa <91765187+Mariam85@users.noreply.github.com>
2022-07-04 23:29:47 +0200

    Added the categories functionalities

--- Program.cs
+++ Program.cs
@@ -30,25 +30,29 @@ while (true)
     .InstructionsText("[grey]([blue][/] use up and down arrows to toggle, press space then enter [green][/]to accept)[/]")
     .AddChoices(
         new[]{
-                        "Add","Edit","List","Exit"
+                        "Add recipe","Edit recipe","List recipe","Exit","Remove category","Rename category"
         }));
 
-    string userChoice = functionality.Count == 1 ? functionality[0] : null;
+    var userChoice = functionality.Count == 1 ? functionality[0] : null;
 
     // Adding a new recipe.
-    if (userChoice == "Add")
+    if (userChoice == "Add recipe")
     {
         List<string> recipeIngredients = AnsiConsole.Ask<string>("1)Ingredients: [grey]seperate them by adding a - [/] ").Split("-").ToList();
         string recipeTitle = AnsiConsole.Ask<string>("2)Title: ");
         List<string> recipeInstructions = AnsiConsole.Ask<string>("3)Instructions: [grey]seperate them by adding a - [/] ").Split("-").ToList();
         List<string> recipeCategories = AnsiConsole.Ask<string>("4)Categories: [grey]seperate them by adding a - [/] ").Split("-").ToList();
 
+
         var recipe = new Recipe(recipeIngredients, recipeTitle, recipeInstructions, recipeCategories);
         var options = new JsonSerializerOptions { WriteIndented = true };
         string jsonString = File.ReadAllText(sFilePath);
-        List<Recipe> menu = System.Text.Json.JsonSerializer.Deserialize<List<Recipe>>(jsonString);
-        menu.Add(recipe);
-        File.WriteAllText(sFilePath, System.Text.Json.JsonSerializer.Serialize(menu, options));
+        var menu = JsonSerializer.Deserialize<List<Recipe>>(jsonString);
+        if (menu != null)
+        {
+            menu.Add(recipe);
+            File.WriteAllText(sFilePath, System.Text.Json.JsonSerializer.Serialize(menu, options));
+        }
 
         AnsiConsole.Write("Successfully added the recipe!\n");
         bool mainMenu = AnsiConsole.Confirm("Do you want to return to main menu?");
@@ -60,9 +64,31 @@ while (true)
     }
 
     // Editing a recipe.
-    else if (userChoice == "Edit")
+    else if (userChoice == "Edit recipe")
     {
-        string title = AnsiConsole.Ask<string>("Enter the title of the recipe to edit:");
+        string jsonString = File.ReadAllText(sFilePath);
+        var menu = System.Text.Json.JsonSerializer.Deserialize<List<Recipe>>(jsonString);
+
+        var table = new Table().Border(TableBorder.Ascii2);
+        table.Expand();
+        table.AddColumn("[dodgerblue2]Title[/]");
+        table.AddColumn(new TableColumn("[dodgerblue2]Ingredients[/]").LeftAligned());
+        table.AddColumn(new TableColumn("[dodgerblue2]Instructions[/]").LeftAligned());
+        table.AddColumn(new TableColumn("[dodgerblue2]Categories[/]").LeftAligned());
+
+        for (int i = 0; i < menu.Count; i++)
+        {
+            table.AddRow(
+                 String.Join("\n", menu[i].Title),
+                 String.Join("\n", menu[i].Ingredients.Select(x => $"- {x}")),
+                 String.Join("\n", menu[i].Instructions.Select((x, n) => $"- {x}")),
+                 String.Join("\n", menu[i].Categories.Select((x) => $"- {x}")));
+            table.AddEmptyRow();
+        }
+        AnsiConsole.Write(table);
+
+        int index = AnsiConsole.Ask<int>("Enter the number of the recipe to edit:");
+        Recipe recipeToEdit = menu[index - 1];
         var attributeToEdit = AnsiConsole.Prompt(
             new MultiSelectionPrompt<string>()
             .PageSize(10)
@@ -74,30 +100,29 @@ while (true)
                                 "categories", "instructions", "ingredients", "title"
                 }));
 
-        string choiceEdit = attributeToEdit.Count == 1 ? attributeToEdit[0] : null;
-        string jsonString = File.ReadAllText(sFilePath);
-        List<Recipe> menu = System.Text.Json.JsonSerializer.Deserialize<List<Recipe>>(jsonString);
+        var choiceEdit = attributeToEdit.Count == 1 ? attributeToEdit[0] : null;
 
         // Checking which element the user would like to edit. 
         if (choiceEdit == "categories")
         {
             List<string> newCategory = AnsiConsole.Ask<string>("Enter new categories: [green]seperate them by adding a dash - [/]?").Split("-").ToList();
-            menu.Find(r => r.Title == title).Categories = newCategory;
+            menu.Find(r => r.Id == recipeToEdit.Id).Categories = newCategory;
+
         }
         else if (choiceEdit == "instructions")
         {
             List<string> newInstructions = AnsiConsole.Ask<string>("Enter new instructions: [green]seperate them by adding a dash - .[/]?").Split("-").ToList();
-            menu.Find(r => r.Title == title).Instructions = newInstructions;
+            menu.Find(r => r.Id == recipeToEdit.Id).Instructions = newInstructions;
         }
         else if (choiceEdit == "ingredients")
         {
             List<string> newIngredients = AnsiConsole.Ask<string>("Enter new ingredients: [green]seperate them by adding a dash - .[/]?").Split("-").ToList();
-            menu.Find(r => r.Title == title).Ingredients = newIngredients;
+            menu.Find(r => r.Id == recipeToEdit.Id).Ingredients = newIngredients;
         }
         else
         {
             string newTitle = AnsiConsole.Ask<string>("Enter new title:");
-            menu.Find(r => r.Title == title).Title = newTitle;
+            menu.Find(r => r.Id == recipeToEdit.Id).Title = newTitle;
         }
 
         // Updating the json file.
@@ -112,34 +137,100 @@ while (true)
         AnsiConsole.Clear();
 
     }
-    // Listing a recipe.
-    else if (userChoice == "List")
+    // Lists all recipes with the same title.
+    else if (userChoice == "List recipe")
     {
         string jsonString = File.ReadAllText(sFilePath);
         List<Recipe> menu = System.Text.Json.JsonSerializer.Deserialize<List<Recipe>>(jsonString);
         string listTitle = AnsiConsole.Ask<string>("Enter the title of the recipe to list:");
-        Recipe foundRecipe = menu.Find(r => r.Title == listTitle);
+        List<Recipe> foundRecipe = menu.FindAll(r => r.Title == listTitle);
 
-        AnsiConsole.MarkupLine("[green]Ingredients: [/]");
-        for (int i = 0; i < foundRecipe.Ingredients.Count; i++)
-            AnsiConsole.Write(foundRecipe.Ingredients[i] + " ");
-
+        // Recipe attributes displayed in a table.
+        var table = new Table().Border(TableBorder.Ascii2);
+        table.Expand();
+        table.AddColumn("[dodgerblue2]Title[/]");
+        table.AddColumn(new TableColumn("[dodgerblue2]Ingredients[/]").LeftAligned());
+        table.AddColumn(new TableColumn("[dodgerblue2]Instructions[/]").LeftAligned());
+        table.AddColumn(new TableColumn("[dodgerblue2]Categories[/]").LeftAligned());
+        for (int i = 0; i < foundRecipe.Count; i++)
+        {
+            table.AddRow(
+                 String.Join("\n", foundRecipe[i].Title),
+                 String.Join("\n", foundRecipe[i].Ingredients.Select(x => $"- {x}")),
+                 String.Join("\n", foundRecipe[i].Instructions.Select((x, n) => $"- {x}")),
+                 String.Join("\n", foundRecipe[i].Categories.Select((x) => $"- {x}")));
+            table.AddEmptyRow();
+        }
+        AnsiConsole.Write(table);
         AnsiConsole.Write("\n");
-        AnsiConsole.MarkupLine("[green]Instructions: [/]");
-        for (int i = 0; i < foundRecipe.Instructions.Count; i++)
-            AnsiConsole.Write(foundRecipe.Instructions[i]);
-
-        AnsiConsole.Write("\n");
-        AnsiConsole.MarkupLine("[green]Categories: [/]");
-        for (int i = 0; i < foundRecipe.Categories.Count; i++)
-            AnsiConsole.Write(foundRecipe.Categories[i]);
-
         bool mainMenu = AnsiConsole.Confirm("Do you want to return to main menu?");
         if (!mainMenu)
         {
             break;
         }
         AnsiConsole.Clear();
+    }
+    // Renames a category.
+    else if (userChoice == "Rename category")
+    {
+        string jsonString = File.ReadAllText(sFilePath);
+        var menu = System.Text.Json.JsonSerializer.Deserialize<List<Recipe>>(jsonString);
+        string category = AnsiConsole.Ask<string>("Enter the name of the category to edit:");
+        string newCategory = AnsiConsole.Ask<string>("Enter the category's new name:");
+        List<Recipe> beforeRename = menu.FindAll(r => r.Categories.Contains(category));
+        if (beforeRename.Any())
+        {
+            foreach (Recipe r in beforeRename)
+            {
+                r.Categories.Remove(category);
+                r.Categories.Add(newCategory);
+            }
+            // Updating the json file.
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            File.WriteAllText(sFilePath, System.Text.Json.JsonSerializer.Serialize(menu));
+            AnsiConsole.Write("Successfully removed the category!\n");
+        }
+        else
+        {
+            AnsiConsole.Write("This category was not found\n");
+        }
+        bool mainMenu = AnsiConsole.Confirm("Do you want to return to main menu?");
+        if (!mainMenu)
+        {
+            break;
+        }
+        AnsiConsole.Clear();
+    }
+    // Removes all recipes associated with a specific category.
+    else if (userChoice == "Remove category")
+    {
+        string jsonString = File.ReadAllText(sFilePath);
+        var menu = System.Text.Json.JsonSerializer.Deserialize<List<Recipe>>(jsonString);
+        string category = AnsiConsole.Ask<string>("Enter the name of the category to remove:");
+        int removed = menu.RemoveAll(r => r.Categories.Contains(category));
+        if (removed > 0)
+        { 
+            // Updating the json file.
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            File.WriteAllText(sFilePath, System.Text.Json.JsonSerializer.Serialize(menu));
+            AnsiConsole.Write("Successfully remooved the category!\n");
+            bool mainMenu = AnsiConsole.Confirm("Do you want to return to main menu?");
+            if (!mainMenu)
+            {
+                break;
+            }
+            AnsiConsole.Clear();
+        }
+        else
+        {
+            AnsiConsole.Write("This category was not found\n");
+            bool mainMenu = AnsiConsole.Confirm("Do you want to return to main menu?");
+            if (!mainMenu)
+            {
+                break;
+            }
+            AnsiConsole.Clear();
+        }
     }
     // Exiting.
     else
